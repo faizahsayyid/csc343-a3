3ab4188cbdfd9ecec73cecd9b52c9601ef42f2e9
faizahsayyid <faizah.sayyid@gmail.com>
2022-11-24 17:58:48 -0500

    feat: q1

---
+++ q1.sql
@@ -0,0 +1,25 @@
+-- For each concert, report the total value of all tickets sold and the 
+-- percentage of the venue that was sold.
+
+SET SEARCH_PATH TO concert;
+
+DROP VIEW IF EXISTS 
+    SeatPrices,
+    TotalConcertSale
+CASCADE;
+
+
+-- Define views for your intermediate steps here:
+CREATE VIEW SeatPrices AS 
+    SELECT p.concert_id, p.seat_id, sp.price FROM Purchase p 
+        JOIN Seat s ON s.seat_id = p.seat_id
+        JOIN SectionPrice sp ON s.section_id = sp.section_id 
+        WHERE p.concert_id = c.concert_id;
+
+CREATE VIEW TotalConcertSale AS 
+    SELECT c.concert_id, SUM(sp.price) AS "total" FROM Concert c
+        LEFT JOIN SeatPrices ON sp.concert_id = c.concert_id
+        GROUP BY c.concert_id;
+
+-- OUTPUT
+SELECT * FROM TotalConcertSale;
